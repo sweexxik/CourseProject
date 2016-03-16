af42d2f227dbe9acf9e24f9684315a5352bac148
Aliaksei Rubleuski <sweexxik@gmail.com>
2016-03-16 23:56:43 +0300

    initial commit

---
+++ CourseProject/Providers/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using CourseProject.UserEntities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.OAuth;
+
+namespace CourseProject.Providers
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            //todo find more info about this method
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            using (var repo = new AuthRepository())
+            {
+                var user = await repo.FindUser(context.UserName, context.Password);
+
+                if (user == null)
+                {
+                    context.SetError("invalid_grant", "The user name or password is incorrect.");
+                    return;
+                }
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+
+        }
+    }
+}
---
+++ CourseProject/UserEntities/AuthRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using CourseProject.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CourseProject.UserEntities
+{
+    public class AuthRepository : IDisposable
+    {
+        private readonly AuthContext db;
+
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthRepository()
+        {
+            db = new AuthContext();
+            userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(db));
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserModel userModel)
+        {
+            var user = new IdentityUser
+            {
+                UserName = userModel.UserName
+            };
+
+            var result = await userManager.CreateAsync(user, userModel.Password);
+
+            return result;
+        }
+
+        public async Task<IdentityUser> FindUser(string userName, string password)
+        {
+            var user = await userManager.FindAsync(userName, password);
+
+            return user;
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+            userManager.Dispose();
+
+        }
+    }
+}
