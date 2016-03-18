524afdc3b894e6000cae00eb73a4c9414928e27d
Aliaksei Rubleuski <sweexxik@gmail.com>
2016-03-19 00:03:49 +0300

    drag&drop chapters corresponded with chapter.number field

--- CourseProject.Web/app/controllers/editCreativeController.js
+++ CourseProject.Web/app/controllers/editCreativeController.js
@@ -1,11 +1,84 @@
 'use strict';
-app.controller('editCreativeController', ['$scope','$routeParams','creativeService',
-	function ($scope, $routeParams,creativeService) {
+app.controller('editCreativeController', ['$http','$scope','$routeParams','creativeService',
+	function ($http, $scope, $routeParams,creativeService) {
  		
  		$scope.creative = []; 	
- 		$scope.chapters = []; 			
- 		
-        var sortObject = function(o) {
+ 		$scope.chapters = []; 
+ 		$scope.stub = []; 	
+ 		$scope.chapterModel = {
+	    	body:"",
+	        creativeId:0,    	
+	    	id:0,
+	        name:"",
+	        number:0  
+   		};
+        
+
+		$scope.click = function(item){			
+        	 $scope.selectedchapter = item;
+        	 console.log($scope.selectedchapter);  
+        }
+
+ 		var creativeId = $routeParams.Id;
+
+ 		creativeService.getCreative(creativeId).then(function (results) {
+            $scope.creative = results.data;
+            $scope.chapters = sortObject(results.data); 
+      		console.log(results.data);       
+        }, function (error) {
+            console.log(error);
+        });
+
+        $scope.dropCallback = function(event, index, item, external, type, allowedType) {  
+        	$scope.selectedchapter = undefined; 
+	       	console.log(index);	 
+	       	var startIndex;
+	        for (var chapter in $scope.chapters){
+	        	if ($scope.chapters[chapter].id === item.id){
+	        		console.log("URA");
+	        		startIndex = $scope.chapters[chapter].number;
+	        		$scope.chapters[chapter].number = index + 1;	
+	        	//	$scope.selectedchapter = $scope.chapters[chapter];
+	        		item.number = index + 1;   		
+	        	}
+	        }
+	         for (var i = index; i < startIndex; i++) {
+	        		console.log(i);
+	        		$scope.chapters[i].number++;
+	        }
+        	        
+	        return item;     
+   		};
+		$scope.newChapter = function(){
+			 $scope.selectedchapter = {};
+		}
+
+   		$scope.saveChapter = function(){
+    
+	        initModel();
+
+	        var data = $scope.chapterModel;
+	        console.log(data);
+	        $http.post('http://localhost:57507/api/chapters', JSON.stringify(data), {
+	             headers: { contentType: 'application/json; charset=utf-8', dataType: "json" } }).success(function (response) {
+	                    console.log(response);
+	                    if (response.status == 200) $scope.show = false;
+	            }).error(function (err, status) {
+	            console.log(err);
+	            console.log(status);
+	            });
+	    };
+
+
+	    var initModel = function(){
+	    	$scope.chapterModel.body = $scope.selectedchapter.body;
+	        $scope.chapterModel.number = $scope.selectedchapter.number;
+	        $scope.chapterModel.id = $scope.selectedchapter.id;
+	        $scope.chapterModel.creativeId = $scope.selectedchapter.creativeId;
+	        $scope.chapterModel.name = $scope.selectedchapter.name;
+	    };
+
+   		var sortObject = function(o) {
 		    var sorted = {},
 		    a = [];		
 		    for (var chapter in o.chapters) {		    	
@@ -16,32 +89,5 @@ app.controller('editCreativeController', ['$scope','$routeParams','creativeServi
 		    a.sort(function(x,y){		    	
 		    	return x.number - y.number;
 		    });	  
-		    return a;
-	  
-		}
-
-		$scope.click = function(id){
-        	 $scope.selectedchapter = $scope.creative.chapters[id];
-        	 console.log($scope.selectedchapter);
-        	
-			
-        }
-
- 		var creativeId = $routeParams.Id;
-
- 		creativeService.getCreative(creativeId).then(function (results) {
-
-            $scope.creative = results.data;
-            $scope.chapters = sortObject(results.data); 
-      		console.log(results.data);
-       
-        }, function (error) {
-            console.log(error);
-        });
-
-        $scope.dropCallback = function(event, index, item, external, type, allowedType) {   
-	        console.log(item);
-	        console.log(index);	        
-	        return item;     
-   		};
+		    return a;}
 }]);
--- CourseProject.Web/app/controllers/homeController.js
+++ CourseProject.Web/app/controllers/homeController.js
@@ -5,75 +5,17 @@ app.controller('homeController',
 
  	$scope.authentication = authService.authentication;
     $scope.creatives = [];
-    $scope.show = false;
-    $scope.showAddForm = false;
-    $scope.chapterModel = {
-    	body:"",
-        creativeId:0,    	
-    	id:0,
-        name:"",
-        number:0  
-    };
-
-    $scope.creativeId = "";
-
 
     if(authService.authentication.isAuth){
-    	 creativeService.getCreatives().then(function (results) {
-
+    	creativeService.getCreatives().then(function (results) {
             $scope.creatives = results.data;
-      		console.log(results.data);
-       
+      		console.log(results.data);       
         }, function (error) {
             console.log(error);
         });
-    }
-   
-
-    $scope.getThing = function(id,index){
-    	$scope.creativeId = id;
-    	console.log(index);
-    	var selectedchapter = $scope.creatives[id];     	
-    	$scope.selectedchapter = selectedchapter.chapters[index];
-    	console.log($scope.selectedchapter);    
-    		$scope.show = true;
     }
 
     $scope.newCreative = function(){
     	$location.path('/newCreative');
     }
-
-    $scope.saveChapter = function(){
-    
-        $scope.chapterModel.body = $scope.selectedchapter.body;
-        $scope.chapterModel.number = $scope.selectedchapter.number;
-        $scope.chapterModel.id = $scope.selectedchapter.id;
-        $scope.chapterModel.creativeId = $scope.selectedchapter.creativeId;
-        $scope.chapterModel.name = $scope.selectedchapter.name;
-
-        var data = $scope.chapterModel;
-        console.log(data);
-        $http.post('http://localhost:57507/api/chapters', JSON.stringify(data), {
-             headers: { contentType: 'application/json; charset=utf-8', dataType: "json" } }).success(function (response) {
-                    console.log(response);
-                    if (response.status == 200) $scope.show = false;
-            }).error(function (err, status) {
-            console.log(err);
-            console.log(status);
-            });
-    };
-
-    $scope.dropCallback = function(event, index, item, external, type, allowedType) {
-      //  $scope.logListEvent('dropped at', event, index, external, type);
-        console.log(item);
-        console.log(index);
-        // if (external) {
-        //     if (allowedType === 'itemType' && !item.label) return false;
-        //     if (allowedType === 'containerType' && !angular.isArray(item)) return false;
-        // }
-        return item;
-    };
-
-   
-
 }]);
